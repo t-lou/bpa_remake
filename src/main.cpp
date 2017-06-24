cd2b06306ec58a596615be26b5247fb0e32e135e
Tongxi Lou <tongxi.lou@tum.de>
2017-06-24 18:14:11 +0200

    generic io. next thing is comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include <pcl/common/common_headers.h>
 #include <pcl/PolygonMesh.h>
@@ -7,15 +8,34 @@
 
 #include "BpaRemake"
 
-template <class T>
-typename pcl::PointCloud<T>::Ptr load_ply(const std::string &filename)
+bool is_normal_in_field(const pcl::PCLPointCloud2::ConstPtr &cloud2)
 {
-  pcl::PCLPointCloud2::Ptr cloud_in(new pcl::PCLPointCloud2());
-  typename pcl::PointCloud<T>::Ptr cloud(new pcl::PointCloud<T>());
-  pcl::PLYReader reader;
-  reader.read(filename, *cloud_in);
-  pcl::fromPCLPointCloud2<T>(*cloud_in, *cloud);
-  return cloud;
+  const std::string target = "normal";
+  for(size_t id = 0; id < cloud2->fields.size(); ++id)
+  {
+    if(cloud2->fields.at(id).name.find(target) != std::string::npos)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+template<class T>
+typename pcl::PointCloud<T>::Ptr
+filter_nan(const typename pcl::PointCloud<T>::ConstPtr &in)
+{
+  typename pcl::PointCloud<T>::Ptr out(new typename pcl::PointCloud<T>());
+  out->reserve(in->size());
+  for(size_t id = 0; id < in->size(); ++id)
+  {
+    const T &pt = in->at(id);
+    if(!isnan(pt.x) && !isnan(pt.y) && !isnan(pt.z))
+    {
+      out->push_back(pt);
+    }
+  }
+  return out;
 }
 
 pcl::PCLPointCloud2::Ptr load_pointcloud2(const std::string &filename)
@@ -26,18 +46,38 @@ pcl::PCLPointCloud2::Ptr load_pointcloud2(const std::string &filename)
   return cloud;
 }
 
+pcl::PointCloud<pcl::PointNormal>::Ptr load_cloud(const std::string &filename,
+                                                  const double radius_mls)
+{
+  pcl::PointCloud<pcl::PointNormal>::Ptr re;
+  pcl::PCLPointCloud2::Ptr cloud2 = load_pointcloud2(filename);
+  if(is_normal_in_field(cloud2))
+  {
+    re = pcl::PointCloud<pcl::PointNormal>::Ptr(new pcl::PointCloud<pcl::PointNormal>());
+    pcl::fromPCLPointCloud2<pcl::PointNormal>(*cloud2, *re);
+    re = filter_nan<pcl::PointNormal>(re);
+  }
+  else
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::fromPCLPointCloud2<pcl::PointXYZ>(*cloud2, *cloud_xyz);
+    cloud_xyz = filter_nan<pcl::PointXYZ>(cloud_xyz);
+    re = MlsApplier::smooth<pcl::PointXYZ>(cloud_xyz, radius_mls, true);
+  }
+  return re;
+}
+
 int main(int argc, char **argv)
 {
-  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_in
-      = load_ply<pcl::PointNormal>("/home/tlou/12.2622_-14.3964_1.ply");
-  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in_o
-  //    = load_ply<pcl::PointXYZ>("/home/tlou/bun_zipper.ply");
-  //pcl::PointCloud<pcl::PointNormal>::Ptr cloud_in
-  //    = MlsApplier::smooth<pcl::PointXYZ>(cloud_in_o, 0.01, true);
-  //Pivoter pivoter(cloud_in, 0.02);
+  const std::string fn_in = argv[1];
+  const std::string fn_out = argv[2];
+  const double radius = atof(argv[3]);
+  const double radius_mls = argc >= 5 ? atof(argv[4]) : radius;
+
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_in = load_cloud(fn_in, radius_mls);
   Pivoter pivoter;
 
-  pcl::PolygonMesh::Ptr mesh = pivoter.proceed(cloud_in, 0.03);
-  pcl::io::savePLYFileBinary("/home/tlou/mesh1.ply", *mesh);
+  pcl::PolygonMesh::Ptr mesh = pivoter.proceed(cloud_in, radius);
+  pcl::io::savePLYFileBinary(fn_out, *mesh);
   return 0;
 }
